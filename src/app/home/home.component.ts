767dfaef5e454a3766931963dabdb21f1b87c2db
votienkhiem <votienkhiem@users.noreply.github.com>
2023-06-20 09:32:10 +0700

    a

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,10 +10,43 @@ export class HomeComponent {
   public title = 'Vui lòng nhập vào đây !';
   public age = 15;
   public tin = 12;
+  public fruits = ['apple', 'orange', 'mango'];
+  public fruits2 = [
+    {
+      id: 0,
+      name: 'apple',
+      price: 3000,
+      discount : true
+    },
+    {
+      id: 0,
+      name: 'orange',
+      price: 7500,
+      discount :false
+    },
+    {
+      id: 0,
+      name: 'mango',
+      price: 4000,
+      discount :false
+    },
+    {
+      id: 0,
+      name: 'mangoteen',
+      price: -999,
+      discount :true
+    }
+  ];
+
+
+
 
   public resetName(): void {
     console.log('Tui o day ne !!!')
     this.name = '';
   }
-
+  public ngOnInit(): void {
+    console.log('trai cay = ', this.fruits)
+    console.log(this.fruits2)
+  }
 }
