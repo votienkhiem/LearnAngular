4f07a2acd5f7663fd09e37e1e59cbf40e617c9e2
votienkhiem <votienkhiem@users.noreply.github.com>
2023-06-20 14:15:11 +0700

    q

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,35 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { OnSalePipe } from './on-sale.pipe';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 
+
+const routes: Routes = [
+  { path: '', component: AppComponent },
+  { path: 'home', component: HomeComponent },
+  { path: '**', component: PageNotFoundComponent }, //404
+
+]
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     OnSalePipe,
+    PageNotFoundComponent,
 
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule,
+    [RouterModule.forRoot(routes)],
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [RouterModule]
+
 })
 export class AppModule { }
+
