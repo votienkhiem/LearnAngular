6b061f487981a0a3a4bd8ea5dbf907b9e142d04c
votienkhiem <votienkhiem@users.noreply.github.com>
2023-06-20 12:23:33 +0700

    a

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,27 +16,68 @@ export class HomeComponent {
       id: 0,
       name: 'apple',
       price: 3000,
-      discount : true
+      discount: true
     },
     {
       id: 0,
       name: 'orange',
       price: 7500,
-      discount :false
+      discount: false
     },
     {
       id: 0,
       name: 'mango',
       price: 4000,
-      discount :false
+      discount: false
     },
     {
       id: 0,
       name: 'mangoteen',
       price: -999,
-      discount :true
+      discount: true
     }
   ];
+
+  public districtsAndProvince: string[] = ['Quận / Huyện'];
+  public cities = [
+    {
+      nameCity: 'Chọn Tỉnh / Thành phố',
+      districts: ['Quận / Huyện']
+    },
+    {
+      nameCity: 'Tây Ninh',
+      districts:
+        [
+          'Thành phố Tây Ninh',
+          'Huyện Bến Cầu',
+          'Huyện Gò Dầu',
+          'Huyện Châu Thành',
+          'Huyện Dương Minh Châu',
+          'Huyện Hòa Thành',
+          'Huyện Tân Biên',
+          'Huyện Tân Châu',
+          'Huyện Trảng Bàng'
+        ]
+    },
+    {
+      nameCity: 'An Giang',
+      districts:
+        [
+          'Thành phố Long Xuyên',
+          'Thành phố Châu Đốc',
+          'Thị xã Tân Châu',
+          'Huyện An Phú',
+          'Huyện Châu Phú',
+          'Huyện Châu Thành',
+          'Huyện Chợ Mới',
+          'Huyện Phú Tân',
+          'Huyện Thoại Sơn',
+          'Huyện Tịnh Biên',
+          'Huyện Tri Tôn'
+
+        ]
+    },
+  ]
 
 
 
@@ -48,5 +89,15 @@ export class HomeComponent {
   public ngOnInit(): void {
     console.log('trai cay = ', this.fruits)
     console.log(this.fruits2)
+    console.log(this.cities)
+  }
+  public changeDistricts(event: any): void {
+    const city = event.target.value;
+    console.log('event', city)
+    const search = this.cities.filter((data) => data.nameCity === city)
+
+    if (search && search.length > 0) {
+      this.districtsAndProvince = search[0].districts;
+    }
   }
 }
