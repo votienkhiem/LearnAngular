8475a1aa0005ebd63a550254c543d3b1a6b439ab
votienkhiem <votienkhiem@users.noreply.github.com>
2023-06-28 17:16:50 +0700

    q

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,6 +13,7 @@ export class ProductListComponent {
 
   public share(): void {
     alert('The product has been shared!')
+    console.log(products)
   }
   public onNotify(): void {
     window.alert('You will be notified when the product goes on sale');
