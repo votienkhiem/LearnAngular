edbb618a922112d971f238a15052731fd02af1d4
votienkhiem <votienkhiem@users.noreply.github.com>
2023-06-28 16:32:47 +0700

    a

--- src/app/products.ts
+++ src/app/products.ts
@@ -9,23 +9,58 @@ export interface Product {
 export const products = [
   {
     id: 1,
-    name: 'Phone XL',
+    name: 'Shoe 1',
     price: 799,
     description: 'A large phone with one of the best screens',
     image: 'assets/images/shoe1.jpg'
   },
   {
     id: 2,
-    name: 'Phone Mini',
+    name: 'Shoe 2',
     price: 699,
     description: 'A great phone with one of the best cameras',
     image: 'assets/images/shoe2.jpg'
   },
   {
     id: 3,
-    name: 'Phone Standard',
+    name: 'Shoe 3',
     price: 299,
     description: '',
     image: 'assets/images/shoe3.jpg'
-  }
+  },
+  {
+    id: 4,
+    name: 'Shoe 4',
+    price: 299,
+    description: '',
+    image: 'assets/images/shoe4.jpg'
+  },
+  {
+    id: 5,
+    name: 'Shoe 5',
+    price: 299,
+    description: '',
+    image: 'assets/images/shoe5.jpg'
+  },
+  {
+    id: 6,
+    name: 'Shoe 6',
+    price: 299,
+    description: '',
+    image: 'assets/images/shoe6.jpg'
+  },
+  {
+    id: 7,
+    name: 'Shoe 7',
+    price: 299,
+    description: '',
+    image: 'assets/images/shoe7.jpg'
+  },
+  {
+    id: 8,
+    name: 'Shoe 8',
+    price: 299,
+    description: '',
+    image: 'assets/images/shoe8.jpg'
+  },
 ];
