808249f6065eaa5b16c98d86b5f068ecd5cfb450
votienkhiem <votienkhiem@users.noreply.github.com>
2023-06-28 15:49:44 +0700

    a

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,7 +3,7 @@ export interface Product {
   name: string;
   price: number;
   description: string;
-  image: 'string'
+  image: string;
 }
 
 export const products = [
@@ -12,18 +12,20 @@ export const products = [
     name: 'Phone XL',
     price: 799,
     description: 'A large phone with one of the best screens',
-    image: 'assets/images/'
+    image: 'assets/images/shoe1.jpg'
   },
   {
     id: 2,
     name: 'Phone Mini',
     price: 699,
-    description: 'A great phone with one of the best cameras'
+    description: 'A great phone with one of the best cameras',
+    image: 'assets/images/shoe2.jpg'
   },
   {
     id: 3,
     name: 'Phone Standard',
     price: 299,
-    description: ''
+    description: '',
+    image: 'assets/images/shoe3.jpg'
   }
 ];
