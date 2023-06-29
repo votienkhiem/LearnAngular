09a449680d5ed682e42eb931548669628deb846a
votienkhiem <votienkhiem@users.noreply.github.com>
2023-06-29 13:18:29 +0700

    a

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -25,17 +25,30 @@ export class ProductListComponent {
     window.alert('You will be notified when the product goes on sale');
   }
   public searchItems(): any {
-    const listItem = this.search;
-    const itemName = this.products.filter((data) => data.name === listItem)
-    if (itemName && itemName.length > 0) {
+    // const listItem = this.search;
+    // const itemName = this.products.filter((data) => data.name === listItem)
+    // if (itemName && itemName.length > 0) {
 
-      this.listItems.push(itemName)
+    //   this.listItems.push(itemName)
 
-    } else {
+    //   console.log('name cua search', this.listItems)
+    // } else {
+    //   window.alert("No products found")
+    // }
+    const listFilter = this.search;
+    const productSearch = this.products.filter((data) => data.name === listFilter)
+    if (productSearch && productSearch.length > 0) {
+      this.listItems = productSearch
+    }
+    else {
+
       window.alert("No products found")
     }
+
   }
   ngOnInit() {
+
+
 
   }
 }
